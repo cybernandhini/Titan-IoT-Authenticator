a2f0f3d841ebf93f3160494267abfc52e6350ec3
Nandhini Thiruneelakandan <cybernandhini@gmail.com>
2025-04-12 13:28:57 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,21 +1,14 @@
-<?php
-session_start();
-?>
-
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Titan IoT - Login</title>
+    <title>Titan IoT Login</title>
 </head>
 <body>
-    <h2>Login to Titan IoT Authenticator</h2>
+    <h2>Login</h2>
     <form method="POST" action="login_process.php">
-        <label>Email:</label><br>
-        <input type="email" name="email" required><br><br>
-
-        <label>Password:</label><br>
-        <input type="password" name="password" required><br><br>
-
+        <input type="email" name="email" placeholder="Email" required><br><br>
+        <input type="password" name="password" placeholder="Password" required><br><br>
         <input type="submit" value="Login">
     </form>
 </body>
