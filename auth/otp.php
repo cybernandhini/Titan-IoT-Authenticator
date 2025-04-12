c6da2df95b5917586d28a80b55bd65df21e78c66
Nandhini Thiruneelakandan <cybernandhini@gmail.com>
2025-04-12 05:03:15 +0100

    Update otp.php

--- auth/otp.php
+++ auth/otp.php
@@ -1,3 +1,9 @@
 <?php
-// Generate OTP (session or Twilio)
+require '../config/db.php';
+$otp = rand(100000, 999999);
+$_SESSION['otp'] = $otp;
+$_SESSION['otp_expire'] = time() + 300; // 5 min expiry
+
+// send OTP (Twilio API can replace this echo for real SMS)
+echo "Your OTP is: $otp <a href='verify_otp.php'>Enter OTP</a>";
 ?>
