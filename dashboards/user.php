d18af962656337b4d25557c78943328883bf67f2
Nandhini Thiruneelakandan <cybernandhini@gmail.com>
2025-04-12 05:06:17 +0100

    Update user.php

--- dashboards/user.php
+++ dashboards/user.php
@@ -1,3 +1,6 @@
 <?php
-// IoT User Dashboard (Solar/Grid/H2)
+session_start();
+if ($_SESSION['user']['role'] !== 'user') die('Access denied');
 ?>
+<h2>Welcome User</h2>
+<p>IoT access panel: solar/grid/H2 switch</p>
