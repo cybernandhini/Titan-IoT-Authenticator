f87708bf22917f688d366fab131aa6cac1b4295a
Nandhini Thiruneelakandan <cybernandhini@gmail.com>
2025-04-12 20:24:35 +0100

    Update dp.php

--- config/dp.php
+++ config/dp.php
@@ -4,7 +4,27 @@ $dotenv->load();
 
 $host = $_ENV['DB_HOST'];
 $db   = $_ENV['DB_NAME'];
-$user = $_ENV['DB_USER'];
+$user = $_ENV['DB_USER'];<?php
+$host = 'localhost';
+$db = 'titan_iot';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (PDOException $e) {
+    die("DB connection failed: " . $e->getMessage());
+}
+?>
+
 $pass = $_ENV['DB_PASS'];
 $charset = 'utf8mb4';
 
