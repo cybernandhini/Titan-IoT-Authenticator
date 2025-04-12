d00e0914f0b21c6ed5f57a7fa841f90a429780b9
Nandhini Thiruneelakandan <cybernandhini@gmail.com>
2025-04-12 05:07:01 +0100

    Update employee.php

--- dashboards/employee.php
+++ dashboards/employee.php
@@ -1,3 +1,6 @@
 <?php
-// SME Employee Dashboard
+session_start();
+if ($_SESSION['user']['role'] !== 'employee') die('Access denied');
 ?>
+<h2>Welcome Employee</h2>
+<p>Monitor SME usage, ticketed IoT data</p>
