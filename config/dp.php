6dca3b906538a9d1c7affb7e42c7016b50ecd9c3
Nandhini Thiruneelakandan <cybernandhini@gmail.com>
2025-04-12 05:09:56 +0100

    Update dp.php

--- config/dp.php
+++ config/dp.php
@@ -1,3 +1,8 @@
 <?php
-// PDO connection
+require_once '../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+$dsn = "mysql:host={$_ENV['DB_HOST']};dbname={$_ENV['DB_NAME']};charset=utf8mb4";
+$options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
+$pdo = new PDO($dsn, $_ENV['DB_USER'], $_ENV['DB_PASS'], $options);
 ?>
