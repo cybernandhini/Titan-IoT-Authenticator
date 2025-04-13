73e2ae9e3e0976e1342623906430ac869fc6801d
Nandhini Thiruneelakandan <cybernandhini@gmail.com>
2025-04-13 15:08:56 +0100

    Update dp.php

--- config/dp.php
+++ config/dp.php
@@ -1,5 +1,9 @@
 <?php
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+require_once __DIR__ . '/../vendor/autoload.php'; // Loads Composer autoload
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
 $host = $_ENV['DB_HOST'];
@@ -10,10 +14,11 @@ $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $options = [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES => false,
+    PDO::ATTR_EMULATE_PREPARES   => false,
 ];
+
 try {
     $pdo = new PDO($dsn, $user, $pass, $options);
 } catch (PDOException $e) {
